01f8e267fbe20bcbb609817b27652d666b088046
manoeljr <manoelv8@gmail.com>
2022-10-25 05:49:07 -0300

    Adicionando classes de erros

--- src/main/java/com/educandoweb/services/UserService.java
+++ src/main/java/com/educandoweb/services/UserService.java
@@ -2,6 +2,7 @@ package com.educandoweb.services;
 
 import com.educandoweb.entities.User;
 import com.educandoweb.repositories.UserRepository;
+import com.educandoweb.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -20,7 +21,7 @@ public class UserService {
 
     public User findById(Long id) {
         Optional<User> user = userRepository.findById(id);
-        return user.get();
+        return user.orElseThrow(() -> new ResourceNotFoundException(id));
     }
 
     public User insert(User user) {
